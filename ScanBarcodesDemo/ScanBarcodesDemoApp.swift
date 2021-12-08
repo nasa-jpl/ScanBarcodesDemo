0b580d221a0131b6ecbcd6a561bde7767692d15a
Mark Powell <Mark.W.Powell@jpl.nasa.gov>
2021-12-08 13:41:20 -0800

    Initial Commit

---
+++ ScanBarcodesDemo/ScanBarcodesDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScanBarcodesDemoApp.swift
+//  ScanBarcodesDemo
+//
+//  Created by Mark Powell on 12/8/21.
+//
+
+import SwiftUI
+
+@main
+struct ScanBarcodesDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
